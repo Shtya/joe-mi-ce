e78287fcea5552d0673ed8d4bbee5553a700e555
mostafa <mostafatam79@gmail.com>
2026-01-19 16:20:44 +0200

    update the chains

--- entities/locations/chain.entity.ts
+++ entities/locations/chain.entity.ts
@@ -1,14 +1,15 @@
 import { Branch } from 'entities/branch.entity';
 import { CoreEntity } from 'entities/core.entity';
 import { Project } from 'entities/project.entity';
-import { Entity, Column, OneToMany, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, OneToMany, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, Unique } from 'typeorm';
 
 @Entity('chains')
+@Unique(['name', 'project'])
 export class Chain {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true })
+  @Column()
   name: string;
 
   @Column({ nullable: true })
