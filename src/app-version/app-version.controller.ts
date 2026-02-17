f38af2187c3081181a202bd599e6600a4d7fdfdf
mostafa <mostafatam79@gmail.com>
2026-02-17 04:46:42 +0200

    update

--- src/app-version/app-version.controller.ts
+++ src/app-version/app-version.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppVersionService } from './app-version.service';
 import { CreateAppVersionDto } from './dto/create-app-version.dto';
+import { Permissions } from 'decorators/permissions.decorators';
+import { EPermission } from 'enums/Permissions.enum';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('app-version')
 export class AppVersionController {
@@ -10,7 +14,8 @@ export class AppVersionController {
   getUpdateInfo() {
     return this.appVersionService.getUpdateInfo();
   }
-
+@UseGuards(AuthGuard)
+  @Permissions(EPermission.ROLE_CREATE)
   @Post()
   createVersion(@Body() createAppVersionDto: CreateAppVersionDto) {
     return this.appVersionService.createVersion(createAppVersionDto);
