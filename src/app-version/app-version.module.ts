1a2abc3e1ee27158338d703642f28b85f907caf4
Shady ElDawy <shadyeldawy96@gmail.com>
2026-02-17 07:47:07 +0000

    fix: add User entity to AppVersionModule to resolve AuthGuard dependency

--- src/app-version/app-version.module.ts
+++ src/app-version/app-version.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppVersion } from 'entities/app-version.entity';
 import { AppVersionController } from './app-version.controller';
 import { AppVersionService } from './app-version.service';
+import { User } from 'entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AppVersion])],
+  imports: [TypeOrmModule.forFeature([AppVersion, User])],
   controllers: [AppVersionController],
   providers: [AppVersionService],
 })
