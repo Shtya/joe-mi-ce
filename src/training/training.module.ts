d5412633f3ed5d13105091969028b2af2569d45d
mostafa <mostafatam79@gmail.com>
2026-03-03 21:17:21 +0200

    solve

--- src/training/training.module.ts
+++ src/training/training.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Training } from '../../entities/training.entity';
 import { TrainingService } from './training.service';
 import { TrainingController } from './training.controller';
+import { User } from '../../entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Training])],
+  imports: [TypeOrmModule.forFeature([Training, User])],
   controllers: [TrainingController],
   providers: [TrainingService],
   exports: [TrainingService],
