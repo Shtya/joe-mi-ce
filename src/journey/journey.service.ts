078a5069fd8e35000ef026e3c0d8d0cb68e1df8e
mostafa <mostafatam79@gmail.com>
2026-08-16 20:35:23 +0300

    fix

--- src/journey/journey.service.ts
+++ src/journey/journey.service.ts
@@ -2360,7 +2360,7 @@ export class JourneyService {
         new Brackets((subQb) => {
           subQb
             .where("journey.id IS NULL")
-            .orWhere("journey.projectId != user.project_id::uuid");
+            .orWhere("journey.projectId::text != user.project_id");
         }),
       );
 
